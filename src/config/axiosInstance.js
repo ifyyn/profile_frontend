2564eabf95bc5d556186bb368f3e8d6400176966
Fathul Arifin <fyn.ifyn>
2024-12-30 19:24:43 +0200

    Build production-ready frontend

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const config = {
-  baseURL: "https://web-production-fcd8.up.railway.app/api/v1",
+  baseURL: "https://web-production-fcd8.up.railway.app",
 };
 
 export const axiosInstance = axios.create(config);
